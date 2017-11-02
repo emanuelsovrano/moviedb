4a41229714f08fe45555fc121f189cd75bed0cf3
emanuel.sovrano <emanuel.sovrano@outlook.com>
2017-11-02 14:16:15 +0100

    Änderungen

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,6 @@ function myfirstrequest() {
             document.getElementById("demo").innerHTML = this.responseText;
         }
     };
-    xhhtp.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=ae03a3bcc77aae15f3e3d3bda3d7d325", true);
+    xhhtp.open('GET', 'https://api.themoviedb.org/3/movie/550?api_key=ae03a3bcc77aae15f3e3d3bda3d7d325', true);
     xhhtp.send();
 }
